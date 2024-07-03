3e8058f945d23512fcf1eeb47824710443e8806d
Parag479 <pdubey479@gmail.com>
2024-07-03 17:53:31 +0530

    changes text work

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -25,14 +25,9 @@ const Works = () => {
           </span>
           <span>Brands & Clients</span>
           <spane>
-            Lorem ispum is simpley dummy text of printing of printing Lorem
-            <br />
-            ispum is simpley dummy text of printingLorem ispum is simpley dummy
-            text
-            <br />
-            y dummy text of printingLorem
-            <br />
-            ispum is simpley dummy text of printing
+          Expert in UI/UX design, web development, and branding. Delivered high-quality work for numerous brands and clients.
+
+
           </spane>
           <Link to="contact" smooth={true} spy={true}>
             <button className="button s-button">Hire Me</button>
