2fa45e264f4a01389912aec2de7839e23fd7cbc6
Parag479 <pdubey479@gmail.com>
2024-07-04 12:25:52 +0530

    email changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_2mu5xtl",
-        "template_m5udu2c",
+        "service_xk8l4i2",
+        "template_rxglb2s",
         form.current,
-        "VLwg1ltOWvnCYAiK_"
+        "QyUOKqBeTZWXjMOcQ"
       )
       .then(
         (result) => {
