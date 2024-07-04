66547187e93723150fdf4e68689ab146f5f30680
Parag479 <pdubey479@gmail.com>
2024-07-04 11:21:29 +0530

    changes detail

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import Farmitra from "../../img/Farmitra.png";
 import Gaurs from "../../img/logo-black.png";
 import Tender from "../../img/tender.png";
+import gratex from "../../img/gratex.png";
 import pioneer from "../../img/logo.png";
 
 import { themeContext } from "../../Context";
@@ -20,7 +21,7 @@ const Portfolio = () => {
       {/* slider */}
       <Swiper
         spaceBetween={30}
-        slidesPerView={3}
+        slidesPerView={4}
         grabCursor={true}
         className="portfolio-slider"
       >
@@ -37,6 +38,9 @@ const Portfolio = () => {
         </SwiperSlide>
         <SwiperSlide>
           <img src={Tender} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={gratex} alt="" />
         </SwiperSlide>
         <SwiperSlide>
           <img src={pioneer} alt="" />
