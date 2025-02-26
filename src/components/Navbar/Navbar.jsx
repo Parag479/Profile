b14bb5583b85bc38cad63a612bc2f5146137de7f
parag dubey <pdubey479@gmail.com>
2025-02-26 12:50:40 +0530

    update resume file reference in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import Toggle from "../Toggle/Toggle";
 import "./Navbar.css";
 import { Link } from "react-scroll";
-import Resume from './Parag_UI_UX_Resume.pdf';
+import Resume from './Parag_UI_UX.pdf';
+
+const handleDownload = () => {
+  const link = document.createElement('a');
+  link.href = Resume;
+  link.download = 'Parag_UI_UX.pdf';
+  link.click();
+};
 
 const navbar = () => {
   return (
@@ -45,7 +52,7 @@ const navbar = () => {
         </div>
         {/* <Link to="contact" spy={true} smooth={true}>
         <button className="button n-button">Contact</button> */}
-        <a href={Resume} download>
+        <a onClick={handleDownload}>
           <button className="button s-button">Download CV</button>
         </a>
         {/* </Link> */}
